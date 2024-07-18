0751e6426913238c8641a6fe8d8d219d31e5619b
Seyma Akkus <yusufmuhammedakkus@gmail.com>
2024-07-18 14:43:41 +0300

    java projem güncellendi.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Ogrenci ogrenci=new Ogrenci("Şeyma","Akkuş",25,"Çorum",true,false);
-        Ogrenci ogrenci1=new Ogrenci("Emine","Fıstık",19,"Gaziantep",false,false);
-        Ogrenci ogrenci2=new Ogrenci("Yunus Emre","Akgül",23,"Giresun ",true,true);
-        System.out.println(ogrenci.getAd()+" "+ogrenci.getSoyad());
+
+
     }
 }
